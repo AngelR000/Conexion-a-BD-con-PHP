a20973c5a37a201ffff5fbf89bc4c22c21ecdbd0
AngelR000 <angel01680168@gmail.com>
2022-11-28 10:25:01 -0600

    1.1 Doc

--- index.php
+++ index.php
@@ -12,9 +12,9 @@ $servername = "localhost";
 $database = "mineria";
 $username = "root";
 $password = "";
-// Create connection
+// Crea la conexion
 $conn = mysqli_connect($servername, $username, $password, $database);
-// Check connection
+// Confirma si se conecto correctamente
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }else{
@@ -32,11 +32,14 @@ if (!$conn) {
   </tr>
 
 <?php
-
+//Guardas en una variable el resultado de la consulta
 $result = mysqli_query($conn, "SELECT * FROM usuarios");
 
+//Mientras que existan datos en el resultado de la funcion mysqli anterior, 
+//se guardan en el arreglo $query_data
 while($query_data = mysqli_fetch_row($result)) {
   echo "<tr>";
+  //Visitas cada campo de la BD con cada indice del arreglo
   echo "<td>",$query_data[0], "</td>",
        "<td>",$query_data[1], "</td>",
        "<td>",$query_data[2], "</td>";
@@ -47,6 +50,7 @@ while($query_data = mysqli_fetch_row($result)) {
 </table>
 
 <?php
+//Limpias la variable de la consulta anterior y cierras la conexion a BD
 mysqli_free_result($result);
   mysqli_close($conn);
 
